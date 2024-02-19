de02e1132c0a1b6adb401c00f6d223883a62a7f7
khai96_ <hvksmr1996@gmail.com>
2024-02-19 18:10:03 +0700

    refactor: Remove unused module

--- src/getOrInit.ts
+++
@@ -1,8 +0,0 @@
-export function getOrInit<Key, Value>(map: Map<Key, Value>, key: Key, init: () => Value): Value {
-  if (map.has(key)) return map.get(key)!
-  const value = init()
-  map.set(key, value)
-  return value
-}
-
-export default getOrInit
